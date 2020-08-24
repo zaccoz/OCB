ad56e04b25138512d0a5a016d71cfdd44e9d2edb
Rémy Voet (ryv) <ryv@odoo.com>
2020-08-24 06:45:20 +0000

    [IMP] (sale_)stock: review forecasted information
    Remove the json_forecast of the sale order line,
    it was a duplicate of information with the qty_at_date_widget.
    But improve this one:
    - Use the forecasted move information in case of confirmed SO.
    - Change the icon (i) to chart.
    - Link to the forcasted change to target new forcasted report
    (Unfortunately we loose the feature to see components product forcasted
    of a kit)
    - Change information print in the popover in case of confirmed SO.
    
    Also refactor the json_forcasted of `stock.move` into multiple fields
    to avoid to dump/loads for other model who needed the forcasted data.
    
    task-2324039
    
    closes odoo/odoo#56425
    
    Related: odoo/enterprise#13063
    Signed-off-by: Arnold Moyaux <amoyaux@users.noreply.github.com>

--- addons/sale_stock/models/sale_order.py
+++ addons/sale_stock/models/sale_order.py
@@ -215,13 +215,16 @@ class SaleOrderLine(models.Model):
     is_mto = fields.Boolean(compute='_compute_is_mto')
     display_qty_widget = fields.Boolean(compute='_compute_qty_to_deliver')
 
-    @api.depends('product_id', 'product_uom_qty', 'qty_delivered', 'state', 'product_uom')
+    @api.depends('product_type', 'product_uom_qty', 'qty_delivered', 'state', 'move_ids')
     def _compute_qty_to_deliver(self):
         """Compute the visibility of the inventory widget."""
         for line in self:
             line.qty_to_deliver = line.product_uom_qty - line.qty_delivered
-            if line.state == 'draft' and line.product_type == 'product' and line.product_uom and line.qty_to_deliver > 0:
-                line.display_qty_widget = True
+            if line.state in ('draft', 'sent', 'sale') and line.product_type == 'product' and line.qty_to_deliver > 0:
+                if line.state == 'sale' and not line.move_ids:
+                    line.display_qty_widget = False
+                else:
+                    line.display_qty_widget = True
             else:
                 line.display_qty_widget = False
 
