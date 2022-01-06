6b9224f1b4de0b9781a3e0c3f9af21244c87a85b
Xavier Morel <xmo@odoo.com>
2022-01-06 20:01:35 +0700

    [IMP] avoid cycle on request

--- odoo/http.py
+++ odoo/http.py
@@ -1450,7 +1450,6 @@ class Root(object):
         """
         try:
             httprequest = werkzeug.wrappers.Request(environ)
-            httprequest.app = self
             httprequest.parameter_storage_class = werkzeug.datastructures.ImmutableOrderedMultiDict
             threading.current_thread().url = httprequest.url
 
